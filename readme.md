7fdd43526856643d64351303d853b9b843797756
Martin Kirk Bonde <martin@bonde.dk>
2022-10-19 22:06:55 +0200

    Update readme.md

--- readme.md
+++ readme.md
@@ -6,5 +6,14 @@ This first version contains a single production sector, and no monetary policy o
 
 Approximately 50,000 households and 5,000 firms are simulated on a monthly basis.
 
-The plots below show responses to first a firm destruction shock where 10% of firms are suddenly closed, second, a productivity shock where the productivity of all firms increases 10 % overnight.
-![plot](./images/readme_plot_example.png)
+## Baseline
+The following plots show the baselines of 300 simulations. The red line is the mean value and shaded areas indicate the 95% confidence intervals. The result of single simulation is shown in gray as an example.
+![plot](./images/baseline.svg)
+
+## Firm destruction shock
+Below we show responses to a firm destruction shock where 10% of firms are suddenly closed
+![plot](./images/firm_destruction_shock.svg)
+
+## Firm productivity shock
+Next, responses to a productivity shock where the productivity of all new firms are 10% higher:
+![plot](./images/firm_productivity_shock.svg)
--- test/test-AgentMAKRO.jl
+++ test/test-AgentMAKRO.jl
@@ -28,16 +28,18 @@ function bandedscatter!(y, ylower, yupper; alpha=0.2, color=:Red, kwargs...)
     band!(1:length(y), ylower, yupper; color=(color, alpha))
 end
 
-
-function plt!(simulations, getter; color=MAKRO, title="", kwargs...)
-    y = zip(getter.(simulations)...) |> collect
+function plt!(simulations, getter; color=DREAM, title="", plot_first=true, kwargs...)
+    y = zip(getter.(simulations)...) |> collect;
 
     ppy = Settings.periods_pr_year
-    y = y[1+ppy:end]
+    y = y[1+ppy:end];
 
     # f = Figure()
     # Axis(f[1,1])
     f = bandedline!(mean.(y), quantile.(y, 0.025), quantile.(y, 0.975); color, kwargs...)
+    if plot_first
+        lines!(first.(y); color=RGBAf(0.0, 0.0, 0.0, 0.3))
+    end
 
     # Change x-axis ticks to years instead of months with 6 ticks shown
     years = length(y) ÷ ppy
@@ -50,13 +52,13 @@ function plt!(simulations, getter; color=MAKRO, title="", kwargs...)
 end
 
 function rel_multiplier_plt!(baselines, shocks, getter; kwargs...) 
-    f = plt!(zip(baselines, shocks), x -> (getter(x[2]) ./ getter(x[1]) .- 1) .* 100; kwargs...)
+    f = plt!(zip(baselines, shocks), x -> (getter(x[2]) ./ getter(x[1]) .- 1) .* 100; plot_first=false, kwargs...)
     current_axis().ylabel = "Percentage change from baseline"
     return f
 end
 
 function abs_multiplier_plt!(baselines, shocks, getter; kwargs...) 
-    f = plt!(zip(baselines, shocks), x -> getter(x[2]) .- getter(x[1]); kwargs...)
+    f = plt!(zip(baselines, shocks), x -> getter(x[2]) .- getter(x[1]); plot_first=false, kwargs...)
     current_axis().ylabel = "Absolute difference from baseline"
     return f
 end
@@ -67,55 +69,55 @@ function pct_plt!(baselines, getter; kwargs...)
     return f
 end
 
-function shock_plt(baselines, shocks; start_year=0, end_year=nothing, fontsize=10, kwargs...)
+function shock_plt(baselines, shocks; fontsize=10, kwargs...)
     f = Figure(; fontsize, kwargs...)
     Axis(f[1,1])
-    rel_multiplier_plt!(baselines, shocks, firms; title="Number of firms", start_year, end_year, color=MAKRO)
+    rel_multiplier_plt!(baselines, shocks, firms; title="Number of firms", color=DREAM)
     Axis(f[1,2])
-    abs_multiplier_plt!(baselines, shocks, expected_profitability; title="Expected Sharpe Ratio", start_year, end_year, color=MAKRO)
+    abs_multiplier_plt!(baselines, shocks, expected_profitability; title="Expected Sharpe Ratio", color=DREAM)
     Axis(f[1,3])
-    rel_multiplier_plt!(baselines, shocks, employed; title="Employement", start_year, end_year, color=MAKRO)
+    rel_multiplier_plt!(baselines, shocks, employed; title="Employement", color=DREAM)
 
     Axis(f[2,1])
-    rel_multiplier_plt!(baselines, shocks, sales; title="Sales", start_year, end_year, color=MAKRO)
+    rel_multiplier_plt!(baselines, shocks, sales; title="Sales", color=DREAM)
     Axis(f[2,2])
-    rel_multiplier_plt!(baselines, shocks, sales; start_year, end_year, color=DREAM)
-    rel_multiplier_plt!(baselines, shocks, capacity; title="Supply and sales (red)", start_year, end_year, color=MAKRO)
+    rel_multiplier_plt!(baselines, shocks, sales; color=DREAM)
+    rel_multiplier_plt!(baselines, shocks, capacity; title="Supply (red) and sales (petrol)", color=MAKRO)
     Axis(f[2,3])
-    rel_multiplier_plt!(baselines, shocks, vacancies; title="Vacancies", start_year, end_year, color=MAKRO)
+    rel_multiplier_plt!(baselines, shocks, vacancies; title="Vacancies", color=DREAM)
 
     Axis(f[3,1])
-    rel_multiplier_plt!(baselines, shocks, x -> wage(x) ./ price(x); title="Real wage", start_year, end_year, color=MAKRO)
+    rel_multiplier_plt!(baselines, shocks, x -> wage(x) ./ price(x); title="Real wage", color=DREAM)
     Axis(f[3,2])
-    rel_multiplier_plt!(baselines, shocks, wage; title="Wage", start_year, end_year, color=MAKRO)
+    rel_multiplier_plt!(baselines, shocks, wage; title="Wage", color=DREAM)
     Axis(f[3,3])
-    rel_multiplier_plt!(baselines, shocks, price; title="Price", start_year, end_year, color=MAKRO)
+    rel_multiplier_plt!(baselines, shocks, price; title="Price", color=DREAM)
 
     return f
 end
 
-function baseline_plt(baselines; start_year=0, end_year = nothing, fontsize = 10, kwargs...)
+function baseline_plt(baselines; fontsize = 10, kwargs...)
     f = Figure(; fontsize, kwargs...)
     Axis(f[1,1])
-    plt!(baselines, firms; title="Number of firms", start_year, end_year, color=MAKRO)
+    plt!(baselines, firms; title="Number of firms", color=DREAM)
     Axis(f[1,2])
-    plt!(baselines, expected_profitability; title="Expected Sharpe Ratio", start_year, end_year, color=MAKRO)
+    plt!(baselines, expected_profitability; title="Expected Sharpe Ratio", color=DREAM)
     Axis(f[1,3])
-    plt!(baselines, sales; title="Sales", start_year, end_year, color=MAKRO)
+    plt!(baselines, sales; title="Sales", color=DREAM)
 
     Axis(f[2,1])
-    plt!(baselines, employed; title="Employement", start_year, end_year, color=MAKRO)
+    plt!(baselines, employed; title="Employement", color=DREAM)
     Axis(f[2,2])
-    pct_plt!(baselines, unemployment_rate; title="Unemployment rate", start_year, end_year, color=MAKRO)
+    pct_plt!(baselines, unemployment_rate; title="Unemployment rate", color=DREAM)
     Axis(f[2,3])
-    plt!(baselines, x -> sales(x) ./ productivity(x); title="Sales, detrended", start_year, end_year, color=MAKRO)
+    plt!(baselines, x -> sales(x) ./ productivity(x); title="Sales, detrended", color=DREAM)
 
     Axis(f[3,1])
-    pct_plt!(baselines, x -> annual_growth_rate(wage(x) ./ price(x)); title="Real wage growth", start_year, end_year, color=MAKRO)
+    pct_plt!(baselines, x -> annual_growth_rate(wage(x) ./ price(x)); title="Real wage growth", color=DREAM)
     Axis(f[3,2])
-    pct_plt!(baselines, x -> annual_growth_rate(wage(x)); title="Wage growth", start_year, end_year, color=MAKRO)
+    pct_plt!(baselines, x -> annual_growth_rate(wage(x)); title="Wage growth", color=DREAM)
     Axis(f[3,3])
-    pct_plt!(baselines, x -> annual_growth_rate(price(x)); title="Inflation", start_year, end_year, color=MAKRO)
+    pct_plt!(baselines, x -> annual_growth_rate(price(x)); title="Inflation", color=DREAM)
 
     return f
 end
@@ -125,9 +127,9 @@ baselines = scenario_statistics[zero_shock!];
 using CairoMakie
 CairoMakie.activate!()
 
-baseline_fig = baseline_plt(baselines, start_year=burnin_years)
-firm_destruction_fig = shock_plt(baselines, scenario_statistics[firm_destruction_shock!], start_year=burnin_years-1)
-firm_productivity_fig = shock_plt(baselines, scenario_statistics[firm_productivity_shock!], start_year=burnin_years-1)
+baseline_fig = baseline_plt(baselines)
+firm_destruction_fig = shock_plt(baselines, scenario_statistics[firm_destruction_shock!])
+firm_productivity_fig = shock_plt(baselines, scenario_statistics[firm_productivity_shock!])
 
 save("images/baseline.svg", baseline_fig, pt_per_unit=1.0)
 save("images/firm_destruction_shock.svg", firm_destruction_fig, pt_per_unit=1.0)
